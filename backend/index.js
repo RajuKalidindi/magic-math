fdcc4d434532053c4bb9fcecc66b71f6011e047b
Raju Kalidindi <krkraju997@gmail.com>
2025-03-24 18:31:57 +0530

    Change /:n endpoint to /api/:n for vercel

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.get("/", (req, res) => {
 	res.send("Welcome to Magic Math API");
 });
 
-app.get("/:n", (req, res) => {
+app.get("/api/:n", (req, res) => {
 	const n = parseInt(req.params.n, 10);
 
 	if (isNaN(n) || n < 0) {
