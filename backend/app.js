4a0d2fb90f791601f84b21cd9a4b08b1cba1813d
Raju Kalidindi <krkraju997@gmail.com>
2025-03-24 16:28:38 +0530

    Initial working code

---
+++ backend/app.js
@@ -0,0 +1,59 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT;
+
+if (!PORT) {
+	throw new Error("PORT is not defined");
+}
+
+app.use(cors());
+
+const calculateMagicMath = (n) => {
+	// Magic Math formula
+	// magic_math(n) = magic_math(n-2) + magic_math(n-1) + n
+
+	if (n === 0) return 0;
+	if (n === 1) return 1;
+
+	// Initialize variables
+	// prev2 = magic_math(0)
+	// prev1 = magic_math(1)
+	let prev2 = 0,
+		prev1 = 1,
+		result = 0;
+
+	for (let i = 2; i <= n; i++) {
+		result = prev2 + prev1 + i;
+		prev2 = prev1;
+		prev1 = result;
+	}
+	return result;
+};
+
+app.get("/", (req, res) => {
+	res.send("Welcome to Magic Math API");
+});
+
+app.get("/:n", (req, res) => {
+	const n = parseInt(req.params.n, 10);
+
+	if (isNaN(n) || n < 0) {
+		return res
+			.status(400)
+			.json("Invalid input. Please provide a non-negative integer.");
+	}
+
+	try {
+		const result = calculateMagicMath(n);
+		res.json({ result });
+	} catch (error) {
+		res.status(500).json("Error calculating Magic Math");
+	}
+});
+
+app.listen(PORT, () => {
+	console.log(`Magic Math API is running on http://127.0.0.1:${PORT}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import MagicMath from "./components/MagicMath";
+
+function App() {
+	return <MagicMath />;
+}
+
+export default App;
